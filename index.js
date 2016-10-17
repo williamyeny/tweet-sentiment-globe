b74bc26c0ba1d9a1a93f3741c75d16e272bf4f7f
williamyeny <williamyeny@gmail.com>
2016-10-17 14:43:43 -0400

    added twitter capability i think

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var Twitter = require("twitter");
+
+var keys = require("./config.json");
+console.log(keys);
 
 var port = 8080;
+
+var client = new Twitter(keys);
+
+client.get('search/tweets', {q: 'sad%20OR%20depressed%20OR%20unhappy', count: 200}, function(error, tweets, response) {
+  console.log(tweets["statuses"].length);
+
+});
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-
 app.get("/", function(req, res) {
-  res.render('index');
+  res.render('index', {});
 });
 
 app.listen(port, function() {
